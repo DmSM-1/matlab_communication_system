a54f261d1a22f1f05c56e1095a49a983fe38b351
Dmitry <X.x.xh@yandex.ru>
2026-01-15 17:18:50 +0300

    получена оценка частотного сдвига по stf

--- simplest model/STF_Detector.m
+++ simplest model/STF_Detector.m
@@ -5,17 +5,18 @@ classdef STF_Detector < handle
     % 1 - est gain   
     % 2 - wait margin
     % 3 - cfo estimate
-    % 4 - enl estimate
-    % 5 - wait margin end exit
+    % 4 - wait margin end exit
     %======================================================================
 
     properties
         stf
         cfo % carrier frequency offset
+        sto % start time offset
         enl % everage noise level
         gc  % gain control
         stage
         detected
+        buf
         debug
     end
 
@@ -27,10 +28,12 @@ classdef STF_Detector < handle
             end
             obj.stf = stf;
             obj.cfo = 0.0;
+            obj.sto = 0;
             obj.enl = 0.0;
             obj.gc  = 1.0;
             obj.stage = 0;
             obj.detected = 0;
+            obj.buf = [];
             obj.debug = options.debug;
         end
 
@@ -43,26 +46,26 @@ classdef STF_Detector < handle
 
             det_mask_weight = sum(obj.stf.det_mask);
 
-            for i = 1:rel_len %Attention
+            for i = 1:rel_len
                 det_buf = waveform((i-1)*obj.stf.symb_len+1:i*obj.stf.symb_len);
-                det_buf = det_buf(obj.stf.L+1:end);
-                det_buf = fft(det_buf);
+                OFDM_buf = det_buf(obj.stf.L+1:end);
+                OFDM_buf = fft(OFDM_buf);
                 
-                pilot_power = det_buf.*obj.stf.det_mask;
+                pilot_power = OFDM_buf.*obj.stf.det_mask;
 
                 pilot_power = sum(abs(pilot_power.^2));
-                noise_power = sum(abs(det_buf.^2))-pilot_power;
+                noise_power = sum(abs(OFDM_buf.^2))-pilot_power;
 
                 pilot_power = pilot_power/det_mask_weight;
                 noise_power = noise_power/(obj.stf.N-det_mask_weight);
 
-                SNR = pilot_power/noise_power;
+                SNR = db(pilot_power/noise_power);
                 
                 if obj.debug 
-                    fprintf("Symb:%4d Stage %d SNR(dB) %3.3f \n", i, obj.stage, db(SNR)); 
+                    fprintf("Symb:%4d Stage %d SNR(dB) %3.3f \n", i, obj.stage, SNR); 
                 end
 
-                if SNR < obj.stf.det_threshold
+                if SNR < obj.stf.det_threshold && obj.stage ~= 4
                     obj.detected = 0;
                     obj.stage = 0;
                     continue;
@@ -79,13 +82,73 @@ classdef STF_Detector < handle
                     obj.detected = obj.detected + 1;
                     if obj.detected >= obj.stf.margin1
                         obj.detected = 0;
+                        obj.buf = [];
                         obj.stage = 3;
                     end
+                
+                elseif obj.stage == 3
+                    obj.detected = obj.detected + 1;
+                    obj.buf = cat(1, obj.buf, det_buf);
 
+                    if obj.detected == obj.stf.est_symb
+
+                        if obj.debug
+                            fprintf("Start CFO est\n");
+                        end
+
+                        obj.buf = obj.buf(1:end-mod(length(obj.buf), obj.stf.period));
+                        obj.buf = reshape(obj.buf, obj.stf.period, []);
+                        
+                        obj.buf = fft(obj.buf);
+    
+                        Ppos = int32(double(obj.stf.Ppos-1).*double(obj.stf.period)./double(obj.stf.N))+1;
+                        
+                        pilots = obj.buf(Ppos, :);
+                        pilot_phases = unwrap(angle(pilots), [], 2);
+
+                        x = repmat([1:length(pilot_phases)], length(Ppos), 1);
+                        
+                        obj.cfo = polyfit(x, pilot_phases, 1);
+                        obj.cfo = obj.cfo(1)/2/pi/double(obj.stf.period);
+                        
+                        if obj.debug
+                            fprintf("Pilot position: %d\n", Ppos);
+                            fprintf("Rel CFO: %e\n", obj.cfo);
+                            figure(100);
+                            clf;
+                            plot(pilot_phases.');
+                            title('STF Pilot diff phases');
+                            xlabel("smaple");
+                            ylabel("phase");
+                            grid("on");
+                        end
+                        obj.detected = 0;
+                        obj.buf = [];
+                        obj.stage = 4;
+                    end
+
+                elseif obj.stage == 4
+                    obj.detected = obj.detected + 1;
+                    if obj.detected >= obj.stf.margin2
+                        obj.detected = 0;
+                        obj.sto = (i-1)*obj.stf.symb_len+1;
+                        obj.buf = [];
+
+                        if obj.debug
+                            fprintf("STO: %d\n", obj.sto);
+                        end
+                        
+                        break;
+                    end
                 end
             end
-
-            detected = 0;
+            
+            if obj.stage == 4
+                obj.stage = 0;
+                detected = 1;
+            else
+                detected = 0;
+            end
         end
         
     end
--- simplest model/model.m
+++ simplest model/model.m
@@ -6,8 +6,8 @@ clc;
         dist=10, ...
         Fc=2.4e9, ...
         Fs=20e6, ...
-        SNR=10, ...
-        CFO=100);
+        SNR=30, ... %что то здесь не так
+        CFO=1000);
 %==========================================================================
 
 % Frame format ============================================================
@@ -24,19 +24,37 @@ clc;
             est_symb = 4, ...
             margin2 = 1, ...
             det_mask_width = 5, ...
-            det_threshold = 10.0);
+            det_threshold = 10.0); %dB
 %==========================================================================
 
 % Init Reciever ===========================================================
-    stf_detector = STF_Detector(stf, debug=true);
+    stf_detector = STF_Detector(stf, debug=false);
 %==========================================================================
 
 
 rx_waveform = chan.tx(stf.waveform);
 
-stf_detector.detect(rx_waveform);
+detect = stf_detector.detect(rx_waveform);
 
-figure (1);
+fprintf("Frame detected: %d\n", detect);
+
+figure(1);
+clf;
+spectrogram(rx_waveform, 'yaxis', N);
+
+figure(2);
+clf;
+subplot(2,1,1)
+plot(real(rx_waveform));
+
+subplot(2,1,2)
+plot(abs(fftshift(fft(rx_waveform))));
+
+t = 0:length(rx_waveform)-1;
+t = t.';
+rx_waveform = rx_waveform.*exp(-2i*pi*stf_detector.cfo*t);
+
+figure(3);
 clf;
 subplot(2,1,1)
 plot(real(rx_waveform));
